c804453482fdb7ba70204d37194188d1b5ea8f8c
tyao-t <e1124596@u.nus.edu>
2025-09-24 20:56:45 +0800

    Add folder for llama and qwen

---
+++ i_coding/llama_qwen/llama2.py
@@ -0,0 +1,27 @@
+# class GPTModel(nn.Module):
+class Llama2Model(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"], dtype=cfg["dtype"])
+        # self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        # self.drop_emb = nn.Dropout(cfg["drop_rate"])
+
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+
+        ################################### NEW ###################################
+        # self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.final_norm = RMSNorm(cfg["emb_dim"])
+        ###########################################################################
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False, dtype=cfg["dtype"])
+
+    def forward(self, in_idx):
+        # batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        # pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+        x = tok_embeds  # + pos_embeds  # Shape [batch_size, num_tokens, emb_size]
+        # x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
