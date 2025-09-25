4ff227d29d3d572bc9d80ef98f17ff1bab304b2c
tianhao.yao <tianhao.yao@bytedance.com>
2025-09-25 14:09:12 +0800

    Add llama 3 code

--- i_coding/llama_qwen/llama2.py
+++ i_coding/llama_qwen/llama2.py
@@ -1,4 +1,43 @@
 # class GPTModel(nn.Module):
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            dtype=cfg["dtype"]  # NEW
+            # dropout=cfg["drop_rate"],
+            # qkv_bias=cfg["qkv_bias"]
+        )
+        self.ff = FeedForward(cfg)
+        ################################### NEW ###################################
+        # self.norm1 = LayerNorm(cfg["emb_dim"])
+        # self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.norm1 = RMSNorm(cfg["emb_dim"])
+        self.norm2 = RMSNorm(cfg["emb_dim"])
+        ###########################################################################
+
+        # self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+        # Shortcut connection for attention block
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)   # Shape [batch_size, num_tokens, emb_size]
+        # x = self.drop_shortcut(x)
+        x = x + shortcut  # Add the original input back
+
+        # Shortcut connection for feed-forward block
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        # x = self.drop_shortcut(x)
+        x = x + shortcut  # Add the original input back
+
+        return x
+
 class Llama2Model(nn.Module):
     def __init__(self, cfg):
         super().__init__()
---
+++ i_coding/llama_qwen/llama3.py
@@ -0,0 +1,172 @@
+import torch.nn as nn
+class GroupedQueryAttention(nn.Module):
+    def __init__(
+            self, d_in, d_out, num_heads,
+            num_kv_groups,       # NEW
+            dtype=None
+        ):
+        super().__init__()
+        assert d_out % num_heads == 0, "d_out must be divisible by num_heads"
+        assert num_heads % num_kv_groups == 0, "num_heads must be divisible by num_kv_groups"  # NEW
+
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+
+        ############################# NEW  #############################
+        # self.W_key = nn.Linear(d_in, d_out, bias=False, dtype=dtype)
+        # self.W_value = nn.Linear(d_in, d_out, bias=False, dtype=dtype)
+        self.W_key = nn.Linear(d_in, num_kv_groups * self.head_dim, bias=False, dtype=dtype)
+        self.W_value = nn.Linear(d_in, num_kv_groups * self.head_dim, bias=False, dtype=dtype)
+        self.num_kv_groups = num_kv_groups
+        self.group_size = num_heads // num_kv_groups
+        ################################################################
+
+        self.W_query = nn.Linear(d_in, d_out, bias=False, dtype=dtype)
+        self.out_proj = nn.Linear(d_out, d_out, bias=False, dtype=dtype)
+
+
+    def forward(self, x, mask=None, cos=None, sin=None):
+        ##################### NEW  #####################
+        # The forward method now accepts `mask` instead of accessing it via self.mask.
+        # Also, we now have cos and sin as input for RoPE
+        ################################################    
+        b, num_tokens, d_in = x.shape
+
+        queries = self.W_query(x)  # Shape: (b, num_tokens, d_out)
+        keys = self.W_key(x)  # Shape: (b, num_tokens, num_kv_groups * head_dim)
+        values = self.W_value(x)  # Shape: (b, num_tokens, num_kv_groups * head_dim)
+
+        # Reshape queries, keys, and values
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        ##################### NEW  #####################
+        # keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        # values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+        keys = keys.view(b, num_tokens, self.num_kv_groups, self.head_dim)
+        values = values.view(b, num_tokens, self.num_kv_groups, self.head_dim)
+        ################################################
+
+        # Transpose keys, values, and queries
+        keys = keys.transpose(1, 2)  # Shape: (b, num_kv_groups, num_tokens, head_dim)
+        values = values.transpose(1, 2)  # Shape: (b, num_kv_groups, num_tokens, head_dim)
+        queries = queries.transpose(1, 2)  # Shape: (b, num_heads, num_tokens, head_dim)
+
+        ##################### NEW #####################
+        # Apply RoPE
+        if cos is not None:
+            keys = compute_rope(keys, cos, sin)
+            queries = compute_rope(queries, cos, sin)
+        ################################################
+
+        ##################### NEW  #####################
+        # Expand keys and values to match the number of heads
+        # Shape: (b, num_heads, num_tokens, head_dim)
+
+        keys = keys.repeat_interleave(self.group_size, dim=1)  # Shape: (b, num_heads, num_tokens, head_dim)
+        values = values.repeat_interleave(self.group_size, dim=1)  # Shape: (b, num_heads, num_tokens, head_dim)
+        # For example, before repeat_interleave along dim=1 (query groups):
+        #   [K1, K2]
+        # After repeat_interleave (each query group is repeated group_size times):
+        #   [K1, K1, K2, K2]
+        # If we used regular repeat instead of repeat_interleave, we'd get:
+        #   [K1, K2, K1, K2]
+        ################################################
+
+        # Compute scaled dot-product attention (aka self-attention) with a causal mask
+        # Shape: (b, num_heads, num_tokens, num_tokens)
+        attn_scores = queries @ keys.transpose(2, 3)  # Dot product for each head
+
+        ##################### NEW #####################
+        # Create mask on the fly
+        if mask is None:
+            mask = torch.triu(torch.ones(num_tokens, num_tokens, device=x.device, dtype=torch.bool), diagonal=1)
+        ################################################
+    
+        # Use the mask to fill attention scores
+        attn_scores.masked_fill_(mask, -torch.inf)
+
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        assert keys.shape[-1] == self.head_dim
+
+        # Shape: (b, num_tokens, num_heads, head_dim)
+        context_vec = (attn_weights @ values).transpose(1, 2)
+
+        # Combine heads, where self.d_out = self.num_heads * self.head_dim
+        context_vec = context_vec.reshape(b, num_tokens, self.d_out)
+        context_vec = self.out_proj(context_vec)  # optional projection
+
+        return context_vec
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = GroupedQueryAttention(  # MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            num_heads=cfg["n_heads"],
+            num_kv_groups=cfg["n_kv_groups"],  # NEW
+            dtype=cfg["dtype"]
+        )
+        self.ff = FeedForward(cfg)
+        self.norm1 = RMSNorm(cfg["emb_dim"], eps=1e-5)
+        self.norm2 = RMSNorm(cfg["emb_dim"], eps=1e-5)
+
+    def forward(self, x, mask=None, cos=None, sin=None):
+        ##################### NEW  #####################
+        # The forward method now accepts `mask` instead of accessing it via self.mask.
+        # Also, we now have cos and sin as input for RoPE
+        ################################################
+        # Shortcut connection for attention block
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x.to(torch.bfloat16), mask, cos, sin)   # Shape [batch_size, num_tokens, emb_size]
+        x = x + shortcut  # Add the original input back
+
+        # Shortcut connection for feed-forward block
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x.to(torch.bfloat16))
+        x = x + shortcut  # Add the original input back
+
+        return x
+
+class Llama3Model(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"], dtype=cfg["dtype"])
+
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+
+        self.final_norm = RMSNorm(cfg["emb_dim"], eps=1e-5)
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False, dtype=cfg["dtype"])
+
+        #################### NEW #####################
+        cos, sin = precompute_rope_params(
+            head_dim=cfg["emb_dim"] // cfg["n_heads"],
+            theta_base=cfg["rope_base"],
+            context_length=cfg["context_length"],
+            freq_config=cfg["rope_freq"]
+        )
+        
+        self.register_buffer("cos", cos, persistent=False)
+        self.register_buffer("sin", sin, persistent=False)
+        ##############################################
+
+        self.cfg = cfg
+
+    def forward(self, in_idx):
+        tok_embeds = self.tok_emb(in_idx)
+        x = tok_embeds
+
+        #################### NEW #####################
+        num_tokens = x.shape[1]
+        mask = torch.triu(torch.ones(num_tokens, num_tokens, device=x.device, dtype=torch.bool), diagonal=1)
+        ##############################################
+        
+        for block in self.trf_blocks:
+            x = block(x, mask, self.cos, self.sin)
+        x = self.final_norm(x)
+        logits = self.out_head(x.to(self.cfg["dtype"]))
+        return logits
